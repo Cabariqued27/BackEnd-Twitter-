806483455a36f85a3ac61cad0aaeb52094ef9de8
Cabariqued27 <98673133+Cabariqued27@users.noreply.github.com>
2022-11-04 14:30:42 -0500

    Delete postSaved.js

--- Modelos/postSaved.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const pdSchema = new mongoose.Schema(
-  {
-    userId: {type: String,required: true},
-    postId: {type: String,required: true},
-  },
-  { timestamps: { createdAt: "created_at" } }
-);
-module.exports = mongoose.model("PostSaved", pdSchema);
