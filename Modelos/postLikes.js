fbd837424efff97940d7a6231a2b6600fa18f91a
Cabariqued27 <98673133+Cabariqued27@users.noreply.github.com>
2022-11-04 14:37:15 -0500

    Delete Modelos directory

--- Modelos/postLikes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const peSchema = new mongoose.Schema(
-  {
-    userId: {type: String,required: true},
-    postId: {type: String,required: true},
-  },
-  { timestamps: { createdAt: "created_at" } }
-);
-module.exports = mongoose.model("PostLike", peSchema);
